05aad1487e4c523ddc508899779d7b85fa257bab
Kosarev <kosarevn97@gmail.com>
2022-10-17 02:05:52 +0700

    ДЗ

--- src/main/java/com/example/homework214/InvalidIndexException.java
+++ src/main/java/com/example/homework214/InvalidIndexException.java
@@ -1,2 +1,23 @@
-package com.example.homework214;public class InvalidIndexException {
+package com.example.homework214;
+
+public class InvalidIndexException extends RuntimeException {
+
+    public InvalidIndexException() {
+    }
+
+    public InvalidIndexException(String message) {
+        super(message);
+    }
+
+    public InvalidIndexException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public InvalidIndexException(Throwable cause) {
+        super(cause);
+    }
+
+    public InvalidIndexException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
 }
